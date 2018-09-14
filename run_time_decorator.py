95eb20345244b608f225561b605d74802746c39b
claudemao <claudemao0131@gmail.com>
2018-09-14 22:36:58 +0100

    带参数的计算时间装饰器

--- run_time_decorator.py
+++ run_time_decorator.py
@@ -1,4 +1,5 @@
 from time import clock
+import functools
 clock()
 '''
 def run_time(func):  
@@ -12,14 +13,17 @@ def run_time(func):
         return func
     return wrapper
 '''
-def run_time(func):
-    def wrapper(*args, **kw):
-        print('“Current Function”:{}, Run time:{}s'.format(func.__name__ ,clock()))
-        return func(*args, **kw) #不加此句或者不加括号内的通畅运行的话会无法实现printt调用
-    return wrapper
-
-@run_time
+def run_time(text):
+    def decorator(func):
+        @functools.wraps(func)    #此处相当于 wrapper.__name__ = func.__name__
+        def wrapper(*args, **kw):
+            print('“ {}, Current Function”:{}, Run time:{}s'.format(text,func.__name__ ,clock()))
+            return func(*args, **kw) #不加此句或者不加括号内的通畅运行的话会无法实现printt调用
+        return wrapper
+    return decorator
+@run_time('cnbb') #括号中为装饰器参数，不带参数的话只用两层嵌套
 def printt():
     print('你妈死了')
 
 printt()
+print(printt.__name__)
