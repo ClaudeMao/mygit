7d2af1c94c47ebfdc460e69582bcf63198b5f418
claudemao <claudemao0131@gmail.com>
2018-09-18 22:18:26 +0100

    add some explanation on function of hasattr and getattr

--- acquire_infor_of_object.py
+++ acquire_infor_of_object.py
@@ -12,10 +12,15 @@ print(eng.speed)
 
 print(hasattr(eng, 'acceleration'))
 print(hasattr(eng, '__acceleration')) #私有变量无法使用hasattr()方法访问
-print(hasattr(eng, '__mass__')
+print(hasattr(eng, '__mass__'))
 
-print(hasattr(eng, 'original_speed')
+print(hasattr(eng, 'original_speed'))
 
 setattr(eng, 'original_speed', 0) #设置一个original_speed属性
-eng.original_speed = 0
-getattr(eng, 'original_speed')
+                                  #上式等效于 eng.original_speed = 0
+print(getattr(eng, 'original_speed'))
+print(getattr(eng, 'blala', 404))
+'''
+其作用在于当调用一个模块中很多方法时，若使用if判断进行调用则太繁琐
+使用hasattr进行检索，使用getattr进行调用会简洁很多
+'''
