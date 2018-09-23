c767b3a0733a1be480317b4d699ccafe1584c049
claudemao <claudemao0131@gmail.com>
2018-09-23 15:52:05 +0100

    use @property

---
+++ how_to_use_@property.py
@@ -0,0 +1,27 @@
+class Screen(object):
+    pass
+    
+    @property
+    def width(self):
+        return self._width
+        
+    @width.setter
+    def width(self, value):
+        self._width = value
+        
+    @property
+    def height(self):
+        return self._height
+        
+    @height.setter
+    def height(self, value):
+        self._height = value
+        
+    @property
+    def resolution(self):
+        return self._width * self._height
+        
+s = Screen()
+s.width = 1024
+s.height = 768
+print('resolution = {}'.format(s.resolution))
