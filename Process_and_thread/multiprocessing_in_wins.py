0efe84e9154e242e95e80623d159669f2fd359f4
claudemao <claudemao0131@gmail.com>
2018-09-30 13:51:39 +0100

    how to use multiprocessing and threads

---
+++ Process_and_thread/multiprocessing_in_wins.py
@@ -0,0 +1,17 @@
+from multiprocessing import Process
+import os 
+#winsdows下无os.fork()
+# fork子进程默认为 pid = 0
+
+def run_proc(name):
+    print('Run child process {}({})'.format(name, os.getpid()))
+    
+if __name__ == '__main__':
+    print('Parent process is {}'.format(os.getpid()))
+    p = Process(target = run_proc, args = ('cnbb',))
+    # args 的参数只要一个值的时候，参数后面需要加一个逗号
+    # 表示args是可迭代的，后面可能还有别的参数，不加逗号会出错
+    print('Child process will start')
+    p.start()
+    p.join()
+    print('Child process end.')
