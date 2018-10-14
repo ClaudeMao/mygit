7b161a6f82c6d99b0942b91a5afa75cd3a44df66
claudemao <claudemao0131@gmail.com>
2018-10-14 16:13:57 +0100

    use struct module to examine whether a pic is a bmp

---
+++ Built-in_module/struct_module.py
@@ -0,0 +1,24 @@
+import base64, struct
+# struct用来处理bytes和其他二进制数据类型的转换
+bmp_data = base64.b64decode('Qk1oAgAAAAAAADYAAAAoAAAAHAAAAAoAAAABABAAAAAAADICAAASCwAAEgsAAAAAAAAAAAAA/3//f/9//3//f/9//3/\
+/f/9//3//f/9//3//f/9//3//f/9//3//f/9//3//f/9//3//f/9//3//f/9/AHwAfAB8AHwAfAB8AHwAfP9//3//fwB8AHwAfAB8/3//f/9/A\
+HwAfAB8AHz/f/9//3//f/9//38AfAB8AHwAfAB8AHwAfAB8AHz/f/9//38AfAB8/3//f/9//3//fwB8AHz/f/9//3//f/9//3//f/9/AHwAfP9//3//f/\
+9/AHwAfP9//3//fwB8AHz/f/9//3//f/9/AHwAfP9//3//f/9//3//f/9//38AfAB8AHwAfAB8AHwAfP9//3//f/9/AHwAfP9//3//f/9//38AfAB8/3\
+//f/9//3//f/9//3//fwB8AHwAfAB8AHwAfAB8/3//f/9//38AfAB8/3//f/9//3//fwB8AHz/f/9//3//f/9//3//f/9/AHwAfP9//3//f/9/AHwAfP\
+9//3//fwB8AHz/f/9/AHz/f/9/AHwAfP9//38AfP9//3//f/9/AHwAfAB8AHwAfAB8AHwAfAB8/3//f/9/AHwAfP9//38AfAB8AHwAfAB8AHwAfAB8/3\
+//f/9//38AfAB8AHwAfAB8AHwAfAB8/3//f/9/AHwAfAB8AHz/fwB8AHwAfAB8AHwAfAB8AHz/f/9//3//f/9//3//f/9//3//f/9//3//f/9//3//f/\
+9//3//f/9//3//f/9//3//f/9//3//f/9//38AAA==')
+print(bmp_data)
+
+def bmp_info(data):
+    useful = struct.unpack('<ccIIIIIIHH', data[:30])
+    if useful[0]==b'B' and useful[1]==b'M':
+        return{
+            'width':useful[6],
+            'height':useful[7],
+            'color':useful[9],
+        }
+    else:
+        print('NotBmp')
+
+print(bmp_info(bmp_data))
