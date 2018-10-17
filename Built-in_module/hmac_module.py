103a8e12f298b6765f19f8d0ce6f773713230dfa
claudemao <claudemao0131@gmail.com>
2018-10-17 21:21:18 +0100

    unfinished hmac algorism

---
+++ Built-in_module/hmac_module.py
@@ -0,0 +1,26 @@
+import hashlib, random
+
+def hmac_md5(key, s):
+    return hmac.new(key.encode('utf-8'), s.encode('utf-8'), 'MD5').hexdigest()
+
+class User(object):
+    def __init__(self, username, password):
+        self.username = username
+        self.key = 'sadness'.join([chr(random.randint(48, 122)) for i in range(20)])
+        self.password = hmac_md5(self.key, password)
+
+db = {
+    'michael': User('michael', '123456'),
+    'bob': User('bob', 'abc999'),
+    'alice': User('alice', 'alice2008')
+}
+
+def login(username, password):
+    user = db[username]
+    MD5PASSWORD = hmac_md5(user.key, password)
+    if MD5PASSWORD == user.password:
+        print('login sucessfully')
+    else:
+        print('Invalid username or password')
+
+login('michael','123456')
