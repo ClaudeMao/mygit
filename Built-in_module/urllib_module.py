258a5d7480090f8c3ae9e11ff34537d422a04103
claudemao <claudemao0131@gmail.com>
2018-10-21 20:31:06 +0100

    use urllib built-in function to do something

--- Built-in_module/urllib_module.py
+++ Built-in_module/urllib_module.py
@@ -1,4 +1,5 @@
 from urllib import request
+import json
 '''
 with request.urlopen('https://api.douban.com/v2/book/2129650') as f:
     data = f.read()
@@ -7,12 +8,25 @@ with request.urlopen('https://api.douban.com/v2/book/2129650') as f:
         print('%s: %s' % (k, v))
     print('Data:', data.decode('utf-8'))
 '''
+'''
 from urllib import request
 
-req = request.Request('http://www.douban.com/')
-req.add_header('User-Agent', 'Mozilla/6.0 (iPhone; CPU iPhone OS 8_0 like Mac OS X) AppleWebKit/536.26 (KHTML, like Gecko) Version/8.0 Mobile/10A5376e Safari/8536.25')
+req = request.Request('https://www.liaoxuefeng.com/')
+req.add_header('User-Agent', 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/69.0.3497.100 Safari/537.36')
 with request.urlopen(req) as f:
     print('Status:', f.status, f.reason)
     for k, v in f.getheaders():
         print('%s: %s' % (k, v))
     print('Data:', f.read().decode('utf-8'))
+'''
+
+def fetch_data(url):
+    with request.urlopen(url) as f:
+        data = f.read()
+        return json.loads(data.decode('utf-8'))
+        
+URL = 'https://query.yahooapis.com/v1/public/yql?q=select%20*%20from%20weather.forecast%20where%20woeid%20%3D%202151330&format=json'
+data = fetch_data(URL)
+print(data)
+assert data['query']['results']['channel']['location']['city'] == 'Beijing'
+print('ok')
