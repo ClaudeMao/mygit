4893b9e9a44f41c6c197ac6649741c4c17bf13a7
claudemao <claudemao0131@gmail.com>
2018-10-24 23:25:24 +0100

    wondering save path

--- Third_party_library/PIL_module.py
+++ Third_party_library/PIL_module.py
@@ -1,5 +1,9 @@
 from PIL import Image
 
-im = Image.open('death.jpg')
+im = Image.open('C:/Users/98415/Desktop/death.jpg')
 w,h = im.size
 print('Original image size:{}*{}'.format(w,h))
+# im.show()
+im.thumbnail((w//2, h//2))
+print('Resize image to:{}*{}'.format(w//2, h//2))
+im.save('thumbnail.jpg', 'jpeg')
