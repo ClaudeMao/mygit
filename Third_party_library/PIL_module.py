2a8fbc50944700116b92ccc7d94f322c82ed2ec5
claudemao <claudemao0131@gmail.com>
2018-10-26 23:15:09 +0100

    change current working directory

--- Third_party_library/PIL_module.py
+++ Third_party_library/PIL_module.py
@@ -1,9 +1,16 @@
 from PIL import Image
+import os
+print(os.getcwd())
+os.chdir('C:/Users/98415/Desktop') # 修改工作目录
+print(os.getcwd())
 
-im = Image.open('C:/Users/98415/Desktop/death.jpg')
+file = '310px-Beta_Negative_Decay.png'
+im = Image.open(file)
+# im.rotate(45).show() # 旋转45度
 w,h = im.size
 print('Original image size:{}*{}'.format(w,h))
-# im.show()
-im.thumbnail((w//2, h//2))
+# im.show() 显示图片
+
+im.thumbnail((w//2,h//2))
 print('Resize image to:{}*{}'.format(w//2, h//2))
-im.save('thumbnail.jpg', 'jpeg')
+im.save('thumbnail.png', 'png')
